23a191eab51f7a6de8f8312651d2addc2984bf9e
n0b0dykn0wsme <95046817+n0b0dykn0wsme@users.noreply.github.com>
2022-03-06 08:46:59 -0800

    added file

---
+++ test.ts
@@ -0,0 +1,11 @@
+(function(){
+    "use strict";
+    /* Start of your code */
+    function greetMe(test) {
+      alert('Test ' + test);
+    }
+  
+    greetMe('test');
+    /* End of your code */
+  })();
+  
